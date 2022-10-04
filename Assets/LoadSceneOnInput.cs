bbcd1fc2dac896e32af4c45a0a58ac51d9d1c8d0
jbponce36 <jbponce36@gmail.com>
2022-10-04 18:10:15 -0300

    Dreadhalls Submission

---
+++ Assets/LoadSceneOnInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Submit") == 1) {
+			
+			if (DontDestroy.instance) {
+				AudioSource music = DontDestroy.instance.GetComponents<AudioSource>()[0];
+				if (!music.isPlaying) {
+					music.Play();
+				}
+			}
+			
+			SceneManager.LoadScene("Play");
+		}
+	}
+}
---
+++ Assets/MazeCount.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class MazeCount : MonoBehaviour {
+
+	private Text text;
+	public static int mazeCount = 1;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text.text = "Maze: " + mazeCount;
+	}
+}
