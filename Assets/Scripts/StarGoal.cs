5570428a246fb555ebf3970012643ad4cff822ac
jbponce36 <jbponce36@gmail.com>
2022-10-21 19:48:00 -0300

    Portal Submission

---
+++ Assets/Scripts/StarGoal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarGoal : MonoBehaviour
+{
+    public bool showText = false;
+
+    void OnTriggerEnter(Collider other) {
+        showText = true;
+	}
+}
---
+++ Assets/Scripts/YouWonText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class YouWonText : MonoBehaviour {
+
+    public GameObject starGoal;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+
+		// start text off as completely transparent black
+		text.color = new Color(0, 0, 0, 0);
+	}
+
+    // Update is called once per frame
+	void Update () {
+		if (starGoal.GetComponent<StarGoal>().showText) {
+            
+            // change text color to orange
+			text.color = new Color(1f, 105f / 255f, 0f, 1f);
+            text.text = "You won!\nPress Return to Restart!";
+            
+            if (Input.GetButtonDown("Submit")) {
+
+                // reload entire scene
+                SceneManager.LoadScene("New Level");
+            }
+		}
+	}
+}
